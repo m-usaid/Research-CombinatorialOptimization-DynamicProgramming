6328c7369d17ee6e16f95ec29985072eb7949bd8
M Usaid Rehman <mr04302@st.habib.edu.pk>
2021-07-19 01:55:19 +0500

    pair work

--- dag.hpp
+++ dag.hpp
@@ -7,6 +7,7 @@ class Node {
 
     public: 
 
+    Node() {}
     Node(const subproblem& s){
         this->subprob = s;
         this->cost = 0;
@@ -15,68 +16,68 @@ class Node {
    
 };
 
-class Edge {
-    Node start, end; 
-    int rigid_index;
+// class Edge {
+//     Node start, end; 
+//     int rigid_index;
 
-    public:
+//     public:
 
-    Edge(const Node& a, const Node& b, int r_i){
-        this->start = a;
-        this->end = b;
-        this->rigid_index = r_i;
-    }
-};
-
-class DAG{
-    int size;
-    int edge_count;
-    std::vector<Node> nodes;
-    std::vector<Edge> edges;
-
-    std::vector<std::vector<Node>> adj_lst;
-
-    public:
-    
-    DAG();
-    DAG(const std::vector<Node>& nodes, const std::vector<Edge>& edges);
-    DAG(const DAG&);
-    ~DAG();
-
-    void construct_adjlist();
-    void print();
-};
-
-// void print_graph(DAG const &graph, int N) {
-//     for (int i=0; i<N; i++) {
-//         std::cout << i << "--->";
-//         for (Matrix v: graph.adj_lst[i]) {
-//             std::cout << v << " ";
-//         }
-//         std::cout << '\n';
+//     Edge(const Node& a, const Node& b, int r_i){
+//         this->start = a;
+//         this->end = b;
+//         this->rigid_index = r_i;
 //     }
+// };
+
+// class DAG{
+//     int size;
+//     int edge_count;
+//     std::vector<Node> nodes;
+//     std::vector<Edge> edges;
+
+//     std::vector<std::vector<Node>> adj_lst;
+
+//     public:
+    
+//     DAG();
+//     DAG(const std::vector<Node>& nodes, const std::vector<Edge>& edges);
+//     DAG(const DAG&);
+//     ~DAG();
+
+//     void construct_adjlist();
+//     void print();
+// };
+
+// // void print_graph(DAG const &graph, int N) {
+// //     for (int i=0; i<N; i++) {
+// //         std::cout << i << "--->";
+// //         for (Matrix v: graph.adj_lst[i]) {
+// //             std::cout << v << " ";
+// //         }
+// //         std::cout << '\n';
+// //     }
+// // }
+
+
+// DAG::DAG() {
+//     size = 0;
+//     edge_count = 0;
 // }
 
+// DAG::DAG(const std::vector<Node>& nodes, const std::vector<Edge>& edges){
+//     this->nodes = nodes;
+//     this->edges = edges;
+//     this->edge_count = this->edges.size();
+//     this->size = this->nodes.size();
 
-DAG::DAG() {
-    size = 0;
-    edge_count = 0;
-}
+// }
 
-DAG::DAG(const std::vector<Node>& nodes, const std::vector<Edge>& edges){
-    this->nodes = nodes;
-    this->edges = edges;
-    this->edge_count = this->edges.size();
-    this->size = this->nodes.size();
+// void DAG::construct_adjlist(){
+    
+// }
 
-}
-
-void DAG::construct_adjlist(){
-
-}
-
-void DAG::print(){
-    for (int i = 0; i < this->size; i++){
-        print()
-    }
-}
+// void DAG::print(){
+//     for (int i = 0; i < this->size; i++){
+//         print()
+//     }
+// }
--- subprobtest.cpp
+++ subprobtest.cpp
@@ -2,13 +2,8 @@
 // create function to generate problem instance 
 
 int main(int argc, char** argv){
-
     MatrixChain mychain = generate_chain(4);
     subproblem subprob = subproblem(1, 4, mychain);
-    // subprob.print_subproblems();
-    // subprob.generate_subproblems();
-    // subprob.print_subproblems();
     subprob.print_subproblems();
-    return 0;
-    
+    return 0;    
 }
